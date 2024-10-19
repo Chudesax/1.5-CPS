fc518eafbdec8566cacc9cf288c2c8a26e0c8cc7
Chudesax <154755482+Chudesax@users.noreply.github.com>
2024-10-19 21:26:55 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 let btnShow = document.querySelector('.brands__show');
-
+let list = document.querySelectorAll('.brands__item--visible');
 btnShow.onclick = function () {
     // Проверяем содержимое текста кнопки
     if (btnShow.textContent == 'Показать все') {
         btnShow.textContent = 'Скрыть';
     } else {
-        btnShow.textContent = 'Показать все';
-    }
+        btnShow.textContent = 'Показать все'
+    };
+
+    btnShow.classList.toggle('arrows-up');
+
+    for (let i = 0; i < list.length; i++) {
+        list[i].classList.toggle('hidden');
+    };
 };
