e28f5641590abc2e9e0eb8d182d2d8f81086a4b6
Chudesax <154755482+Chudesax@users.noreply.github.com>
2024-10-26 12:37:45 +0300

    gorit pukan

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 let btnShow = document.querySelector('.brands__show');
 let list = document.querySelectorAll('.brands__item--visible');
+console.log(list);
+
 btnShow.onclick = function () {
-    // Проверяем содержимое текста кнопки
-    if (btnShow.textContent == 'Показать все') {
-        btnShow.textContent = 'Скрыть';
-    } else {
-        btnShow.textContent = 'Показать все'
-    };
+	if (btnShow.textContent == 'Показать все') {
+		btnShow.textContent = 'Скрыть';
+	} else {
+		btnShow.textContent = 'Показать все';
+	};
 
-    btnShow.classList.toggle('arrows-up');
+	if (btnShow.textContent == 'Скрыть') {
+		btnShow.classList.add('arrows-up')
+	} else {
+		btnShow.classList.remove('arrows-up')
+	};
 
-    for (let i = 0; i < list.length; i++) {
-        list[i].classList.toggle('hidden');
-    };
+	for (let i = 0; i < list.length; i++) {
+		list[i].classList.toggle('hidden');
+		console.log(list[i]);
+	}
 };
