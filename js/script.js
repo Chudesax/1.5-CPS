293e8d9adee027c7ec15be81d382ef221c30f194
Chudesax <154755482+Chudesax@users.noreply.github.com>
2024-12-24 11:40:27 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let btnShow = document.querySelector('.brands__show');
 let list = document.querySelectorAll('.brands__item--visible');
 console.log(list);
 
-btnShow.onclick = function () {
+btnShow.addEventListener('click', function () {
 	if (btnShow.textContent == 'Показать все') {
 		btnShow.textContent = 'Скрыть';
 	} else {
@@ -19,4 +19,41 @@ btnShow.onclick = function () {
 		list[i].classList.toggle('hidden');
 		console.log(list[i]);
 	}
+});
+
+let swiper;
+
+const swiperActivator = function () {
+	if (!swiper) {
+		if (window.innerWidth < 768) {
+			swiper = new Swiper('.swiper', {
+				// Optional parameters
+				direction: 'horizontal',
+				loop: true,
+
+				// If we need pagination
+				pagination: {
+					el: '.swiper-pagination',
+				},
+
+				// Navigation arrows
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+
+				// And if we need scrollbar
+				scrollbar: {
+					el: '.swiper-scrollbar',
+				},
+			});
+		}
+	} else {
+		swiper.destroy(true, true);
+		swiper = null;
+	}
 };
+
+
+window.addEventListener('resize', swiperActivator);
+swiperActivator();
