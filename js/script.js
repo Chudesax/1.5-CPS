e0f44a937e481570de0962f3a0822a06c8ac9656
Chudesax <154755482+Chudesax@users.noreply.github.com>
2024-12-25 15:01:19 +0300

    full upgrade

--- js/script.js
+++ js/script.js
@@ -1,59 +1,33 @@
-let btnShow = document.querySelector('.brands__show');
-let list = document.querySelectorAll('.brands__item--visible');
-console.log(list);
-
-btnShow.addEventListener('click', function () {
-	if (btnShow.textContent == 'Показать все') {
-		btnShow.textContent = 'Скрыть';
-	} else {
-		btnShow.textContent = 'Показать все';
-	};
-
-	if (btnShow.textContent == 'Скрыть') {
-		btnShow.classList.add('arrows-up')
-	} else {
-		btnShow.classList.remove('arrows-up')
-	};
-
-	for (let i = 0; i < list.length; i++) {
-		list[i].classList.toggle('hidden');
-		console.log(list[i]);
-	}
-});
-
 let swiper;
 
 const swiperActivator = function () {
-	if (!swiper) {
-		if (window.innerWidth < 768) {
-			swiper = new Swiper('.swiper', {
-				// Optional parameters
-				direction: 'horizontal',
-				loop: true,
+  if (window.innerWidth < 768) {
+    if (!swiper) {
+      const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 1.5,
+        spaceBetween: 50,
+        breakpoints: {
+          600: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+      });
+    }
+  } else {
+    swiper?.destroy(true, true);
+    swiper = null;
+  }
 
-				// If we need pagination
-				pagination: {
-					el: '.swiper-pagination',
-				},
-
-				// Navigation arrows
-				navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-				},
-
-				// And if we need scrollbar
-				scrollbar: {
-					el: '.swiper-scrollbar',
-				},
-			});
-		}
-	} else {
-		swiper.destroy(true, true);
-		swiper = null;
-	}
 };
 
-
-window.addEventListener('resize', swiperActivator);
 swiperActivator();
+window.addEventListener('resize', swiperActivator);
+
